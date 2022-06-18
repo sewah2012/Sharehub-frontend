beb4f3ba9b062b588027242b6bfbcd7be516e9cf
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2022-06-18 19:38:32 +0100

    update

--- src/pages/confirmEmail/ConfirmEmail.js
+++ src/pages/confirmEmail/ConfirmEmail.js
@@ -6,13 +6,17 @@ import { useNavigate } from "react-router-dom";
 import ConfirmCodePage from "../../components/ConfirmCodePage";
 
 const ConfirmEmail = () => {
+  const type = "verify";
+  const codeMessage =
+    "Please provide the Email Verification code you received in your inbox: ";
+  const confirmMessage = "You email is successfully verified";
 
-    // const url = `/api/auth/verify?code=${code}`;
-    const codeMessage = "Please provide the Email Verification code you received in your inbox: "
-    const confirmMessage = "You email is successfully verified"
-    
   return (
-   <ConfirmCodePage codeMessage={codeMessage} confirmMessage={confirmMessage}/>
+    <ConfirmCodePage
+      codeMessage={codeMessage}
+      confirmMessage={confirmMessage}
+      type={type}
+    />
   );
 };
 
