aa9e7bb9e6fa7c6370be1466c735d68b13ab3ab3
Emmanuel Sahr Sewah <e.sewah@peaqock.com>
2022-06-23 02:04:34 +0100

    update

--- src/pages/MainApp/MainApp.js
+++ src/pages/MainApp/MainApp.js
@@ -14,7 +14,7 @@ const MainApp = () => {
   const [{ currentUserDetails }, dispatch] = useContext(AppContext);
   // console.log(currentUser)
   const [mobile, setMobile] = useState(true);
-  const [loading, setLoading] = useState(false);  
+  const [loading, setLoading] = useState(false);
   // const [regComplete, setRegComplete] = useState(false)
 
   // openShare
@@ -30,56 +30,65 @@ const MainApp = () => {
             type: "LOAD_CURRENT_USER_DETAILS",
             payload: resp.data,
           });
+          setLoading(false);
         }
-        return;
+    
       })
       .catch((err) => {
         console.log(err.response.data);
+        setLoading(false);
       });
 
-    setLoading(false);
+    
     return () => {};
   }, []);
 
   return (
     <div className="mainApp">
       <MainAppBar />
-      
-      {currentUserDetails?.registrationCompleted ? (
-        <section className="mainApp__midSection">
-          <div className="mainApp__midSection-wrapper">
-            <div className="mainApp_shareAddBtn">
-              <Button
-                style={{
-                  backgroundColor: mobile ? "#6F7193" : "#8A8E9D",
-                }}
-                onClick={() => setMobile(!mobile)}
-                variant="contained"
-              >
-                {mobile ? "Add Experience" : "Cancel"}
-              </Button>
-            </div>
-            <div
-              className={
-                mobile
-                  ? "mainApp_midSection_addContainer display"
-                  : "mainApp_midSection_addContainer display openShare"
-              }
-            >
-              <AddExperience open={mobile} setOpen={setMobile} />
-            </div>
-          </div>
 
-          <MidSection />
-
-          <div className="ProfileSection">
-          <ProfileSection />
-          </div>
-        </section>
+      {loading ? (
+        <LinearProgress />
       ) : (
-        <CompleteRegistration />
+        <div>
+          {currentUserDetails?.registrationCompleted ? (
+            <section className="mainApp__midSection">
+              <div className="mainApp__midSection-wrapper">
+                <div className="mainApp_shareAddBtn">
+                  <Button
+                    style={{
+                      backgroundColor: mobile ? "#6F7193" : "#8A8E9D",
+                    }}
+                    onClick={() => setMobile(!mobile)}
+                    variant="contained"
+                  >
+                    {mobile ? "Add Experience" : "Cancel"}
+                  </Button>
+                </div>
+                <div
+                  className={
+                    mobile
+                      ? "mainApp_midSection_addContainer display"
+                      : "mainApp_midSection_addContainer display openShare"
+                  }
+                >
+                  <AddExperience open={mobile} setOpen={setMobile} />
+                </div>
+              </div>
+
+              <MidSection />
+
+              <div className="ProfileSection">
+                <ProfileSection />
+              </div>
+            </section>
+          ) : (
+            <CompleteRegistration />
+          )}
+        </div>
       )}
-      <footer >
+
+      <footer>
         <h4>Footer</h4>
       </footer>
     </div>
