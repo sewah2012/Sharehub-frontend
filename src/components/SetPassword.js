24746ac521a56d4b25413668dda8847cb8fb7af9
Emmanuel Sahr Sewah <sewah2012@gmail.com>
2022-07-03 12:53:16 +0100

    update

--- src/components/SetPassword.js
+++ src/components/SetPassword.js
@@ -1,18 +1,11 @@
 import "./styles/setpassword.css";
 import React, { useContext, useState } from "react";
-import {
-  Button,
-  Chip,
-  CircularProgress,
-  Divider,
-  TextField,
-  Typography,
-} from "@mui/material";
+import { Button, CircularProgress, TextField } from "@mui/material";
 import axios from "axios";
 import { AppContext } from "../states/AppContext";
 
-const SetPassword = ({setSetPassword}) => {
-    const [{currentUserDetails}, dispatch] = useContext(AppContext)
+const SetPassword = ({ setSetPassword }) => {
+  const [{ currentUserDetails }, dispatch] = useContext(AppContext);
   const [isLoading, setisLoading] = useState(false);
   const [newPasswordDetails, setNewPasswordDetails] = useState({
     password: "",
@@ -61,7 +54,7 @@ const SetPassword = ({setSetPassword}) => {
     const url = "/api/auth/newPassword";
     const data = {
       password: newPasswordDetails.password,
-      username: currentUserDetails.username
+      username: currentUserDetails.username,
     };
 
     axios
@@ -69,9 +62,9 @@ const SetPassword = ({setSetPassword}) => {
       .then((resp) => {
         if (resp.status === 200) {
           console.log(resp.data);
-          setSetPassword(false)
+          setSetPassword(false);
           setisLoading(false);
-        } 
+        }
       })
       .catch((err) => {
         console.log(err.response.data);
@@ -79,45 +72,45 @@ const SetPassword = ({setSetPassword}) => {
   };
   return (
     <div className="resetPassword">
-      <Typography variant="h4" component="h2">
-        Please Set your new password and keep it safe.
-      </Typography>
+      <div className="resetPassword__form">
+        <h4>Please Set your new password and keep it safe.</h4>
 
-      <TextField
-        onChange={onChangeHandler}
-        value={newPasswordDetails.password}
-        className="setPasswordTextField"
-        name="password"
-        label="Password"
-        helperText="Please provide a strong password"
-        type="password"
-        style={{ margin: ".1rem" }}
-        error={errors?.password ? errors?.password : false}
-      />
+        <TextField
+          onChange={onChangeHandler}
+          value={newPasswordDetails.password}
+          className="setPasswordTextField"
+          name="password"
+          label="Password"
+          helperText="Please provide a strong password"
+          type="password"
+          style={{ margin: ".1rem" }}
+          error={errors?.password ? errors?.password : false}
+        />
 
-      <TextField
-        onChange={onChangeHandler}
-        value={newPasswordDetails.confirmPassword}
-        className="setPasswordTextField"
-        name="confirmPassword"
-        label="Confirm Password"
-        helperText="Confirm Password must match your password"
-        type="password"
-        style={{ margin: ".1rem" }}
-        error={errors?.confirmPassword ? errors?.confirmPassword : false}
-      />
-      <br />
-      <br />
-      <Button
-        onClick={handleSubmit}
-        variant="contained"
-        disabled={isLoading}
-        fullWidth
-        type="submit"
-      >
-        Save Changes
-        {isLoading && <CircularProgress />}
-      </Button>
+        <TextField
+          onChange={onChangeHandler}
+          value={newPasswordDetails.confirmPassword}
+          className="setPasswordTextField"
+          name="confirmPassword"
+          label="Confirm Password"
+          helperText="Confirm Password must match your password"
+          type="password"
+          style={{ margin: ".1rem" }}
+          error={errors?.confirmPassword ? errors?.confirmPassword : false}
+        />
+        <br />
+        <br />
+        <Button
+          onClick={handleSubmit}
+          variant="contained"
+          disabled={isLoading}
+          fullWidth
+          type="submit"
+        >
+          Save Changes
+          {isLoading && <CircularProgress />}
+        </Button>
+      </div>
     </div>
   );
 };
